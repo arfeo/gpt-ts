ed667cce5549b82c590b111819c6920869cb3df7
Leonid Belikov <mail@arfeo.net>
2019-02-04 00:24:03 +0300

    - default cellSize

--- src/components/Game/index.ts
+++ src/components/Game/index.ts
@@ -1,12 +1,10 @@
-import { DEFAULT_STORAGE_PREFIX, DEFAULT_VMIN_VALUE } from '../../constants/game';
+import { DEFAULT_STORAGE_PREFIX } from '../../constants/game';
 
 import { setUpEventHandlers, removeEventHandlers } from './events';
-import { setCellSize } from './utils';
 
 import { IEventHandler } from '../../types/global';
 
 abstract class GameComponent {
-  cellSize: number;
   eventHandlers: IEventHandler;
   storagePrefix: string;
   init?(...args: any[]): void;
@@ -14,7 +12,6 @@ abstract class GameComponent {
   unmount?(): void;
 
   protected constructor(...args: any[]) {
-    this.cellSize = setCellSize(DEFAULT_VMIN_VALUE);
     this.storagePrefix = DEFAULT_STORAGE_PREFIX;
 
     typeof this.init === 'function' && this.init(...args);
--- src/constants/game.ts
+++ src/constants/game.ts
@@ -1,3 +1,1 @@
-export const DEFAULT_VMIN_VALUE = 5;
-
 export const DEFAULT_STORAGE_PREFIX = 'gpt-ts-game';
