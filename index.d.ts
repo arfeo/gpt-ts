86392b7a6c6d79b8c5c3b1558452d5910892f2aa
Leonid Belikov <mail@arfeo.net>
2019-03-01 06:23:21 +0300

    index.d.ts update

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module 'gpt-ts' {
-  declare class GameComponent {
+  class GameComponent {
     constructor(...args: any[]): void;
     eventHandlers: IEventHandler;
     init(...args: any[]): void;
@@ -10,7 +10,7 @@ declare module 'gpt-ts' {
     removeEventHandlers(): void;
   }
 
-  declare class ModalComponent {
+  class ModalComponent {
     constructor(game: GameComponent, text?: string, size?: 'large' | 'medium' | 'small'): void;
     game: GameComponent;
     modalContainer: HTMLElement;
@@ -23,14 +23,14 @@ declare module 'gpt-ts' {
     close(restoreHandlers?: boolean): void;
   }
 
-  declare class HttpDataSource {
+  class HttpDataSource {
     get(url: string, token: string): Promise<any>;
     post(url: string, data: any, token: string): Promise<any>;
     put(url: string, data: any, token: string): Promise<any>;
     remove(url: string, data: any, token: string): Promise<any>;
   }
 
-  declare function drawCircle(
+  function drawCircle(
     ctx: CanvasRenderingContext2D,
     dotX: number,
     dotY: number,
@@ -40,7 +40,7 @@ declare module 'gpt-ts' {
     edgingColor?: string,
   ): void;
 
-  declare function drawSector(
+  function drawSector(
     ctx: CanvasRenderingContext2D,
     dotX: number,
     dotY: number,
@@ -52,7 +52,7 @@ declare module 'gpt-ts' {
     edgingColor?: string,
   ): void;
 
-  declare function drawArc(
+  function drawArc(
     ctx: CanvasRenderingContext2D,
     cx: number,
     cy: number,
@@ -64,7 +64,7 @@ declare module 'gpt-ts' {
     edgingColor?: string,
   ): void;
 
-  declare function drawLineToAngle(
+  function drawLineToAngle(
     ctx: CanvasRenderingContext2D,
     x1: number,
     y1: number,
@@ -74,7 +74,7 @@ declare module 'gpt-ts' {
     lineWidth: number,
   ): number[][];
 
-  declare function drawRectangle(
+  function drawRectangle(
     ctx: CanvasRenderingContext2D,
     left: number,
     top: number,
@@ -85,7 +85,7 @@ declare module 'gpt-ts' {
     edgingColor?: string,
   ): void;
 
-  declare function drawTriangle(
+  function drawTriangle(
     ctx: CanvasRenderingContext2D,
     c1: number[],
     c2: number[],
@@ -95,7 +95,7 @@ declare module 'gpt-ts' {
     edgingColor?: string,
   ): void;
 
-  declare function drawStar(
+  function drawStar(
     ctx: CanvasRenderingContext2D,
     cx: number,
     cy: number,
@@ -107,18 +107,18 @@ declare module 'gpt-ts' {
     edgingColor?: string,
   ): void;
 
-  declare function lineSegmentsIntersect(segment1: ILineSegment, segment2: ILineSegment): boolean;
-  declare function pointOnLineSegment(segment: ILineSegment, point: IPoint, tolerance: number): boolean;
-  declare function lineSegmentIntersectsWithRect(segment: ILineSegment, rectCoords: number[]): boolean;
+  function lineSegmentsIntersect(segment1: ILineSegment, segment2: ILineSegment): boolean;
+  function pointOnLineSegment(segment: ILineSegment, point: IPoint, tolerance: number): boolean;
+  function lineSegmentIntersectsWithRect(segment: ILineSegment, rectCoords: number[]): boolean;
 
-  declare function getStorageData(key: string): any;
-  declare function saveStorageData(key: string, data: any): void;
-  declare function removeStorageData(key: string): void;
+  function getStorageData(key: string): any;
+  function saveStorageData(key: string, data: any): void;
+  function removeStorageData(key: string): void;
 
-  declare function setCellSize(vmin: number): number;
-  declare function getRandomNum(min = 1, max = 1, discard: number[] = []): number;
+  function setCellSize(vmin: number): number;
+  function getRandomNum(min = 1, max = 1, discard: number[] = []): number;
 
-  declare interface IEventHandler {
+  interface IEventHandler {
     [key: string]: {
       target: HTMLElement;
       type: string;
@@ -126,12 +126,12 @@ declare module 'gpt-ts' {
     }
   }
 
-  declare interface ILineSegment {
+  interface ILineSegment {
     start: IPoint;
     end: IPoint;
   }
 
-  declare interface IPoint {
+  interface IPoint {
     x: number;
     y: number;
   }
