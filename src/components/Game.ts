fe0c33263507ac4ce5801b338b23825135538f4e
Leonid Belikov <mail@arfeo.net>
2019-03-15 13:32:07 +0300

    GameComponent: async init / version up

--- src/components/Game.ts
+++ src/components/Game.ts
@@ -8,19 +8,26 @@ export abstract class GameComponent<T = {}> {
   unmount?(): void;
 
   protected constructor(...args: any[]) {
-    typeof this.init === 'function' && this.init(...args);
-    typeof this.render === 'function' && this.render();
+    this.beforeMount(...args).then(() => {
+      typeof this.render === 'function' && this.render();
 
-    if (Array.isArray(this.eventHandlers) && this.eventHandlers.length > 0) {
-      this.setUpEventHandlers.call(this);
-    }
+      if (Array.isArray(this.eventHandlers) && this.eventHandlers.length > 0) {
+        this.setUpEventHandlers();
+      }
+    });
+  }
+
+  async beforeMount(...args: any[]): Promise<void> {
+    typeof this.init === 'function' && await this.init(...args);
+
+    return Promise.resolve();
   }
 
   destroy() {
     typeof this.unmount === 'function' && this.unmount();
 
     if (Array.isArray(this.eventHandlers) && this.eventHandlers.length > 0) {
-      this.removeEventHandlers.call(this);
+      this.removeEventHandlers();
     }
   }
 
