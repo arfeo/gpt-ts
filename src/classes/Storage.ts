ff97d9b809b8b49dc5bb1c96fc3ec302141b1723
Leonid Belikov <mail@arfeo.net>
2020-03-23 03:10:11 +0300

    Storage class update

--- src/classes/Storage.ts
+++ src/classes/Storage.ts
@@ -1,41 +1,71 @@
+import { Utils } from './Utils';
+
 export class Storage {
   /**
-   * Returns data saved in the local storage under the specified key name
+   * Returns data saved in the local storage under the specified key name;
+   * if the `keys` is an array of key names, it returns the corresponding array
    *
-   * @param key
+   * @param storageName
+   * @param keys
+   * @param onError
    */
-  public static getData(key: string): any {
+  public static getData(storageName: string, keys?: string[] | string, onError?: (error: string) => void): any[] | any | undefined {
     try {
-      return JSON.parse(window.localStorage.getItem(key));
+      const data: any = JSON.parse(window.localStorage.getItem(storageName));
+
+      if (keys === undefined) {
+        return data || {};
+      }
+
+      if (Utils.isObject(data)) {
+        if (Array.isArray(keys)) {
+          return keys.map((key: string): any => data[key]);
+        }
+
+        return data[keys];
+      }
+
+      return Array.isArray(keys) ? [] : undefined;
     } catch (error) {
-      console.error(error);
+      if (typeof onError === 'function') {
+        onError(error);
+      }
     }
   }
 
   /**
    * Saves data to the local storage under the specified key name
    *
+   * @param storageName
    * @param key
    * @param data
+   * @param onError
    */
-  public static saveData(key: string, data: any): void {
+  public static saveData(storageName: string, key: string, data: any, onError?: (error: string) => void): void {
     try {
-      window.localStorage.setItem(key, JSON.stringify(data));
+      window.localStorage.setItem(storageName, JSON.stringify({
+        ...Storage.getStorageData(storageName),
+        [key]: data,
+      }));
     } catch (error) {
-      console.error(error);
+      if (typeof onError === 'function') {
+        onError(error);
+      }
     }
   }
 
   /**
-   * Removes data with the specified key name from the local storage
+   * Clears all data from the local storage
    *
-   * @param key
+   * @param onError
    */
-  public static removeData(key: string): void {
+  public static removeData(onError?: (error: string) => void): void {
     try {
-      window.localStorage.removeItem(key);
+      window.localStorage.clear();
     } catch (error) {
-      console.error(error);
+      if (typeof onError === 'function') {
+        onError(error);
+      }
     }
   }
 }
